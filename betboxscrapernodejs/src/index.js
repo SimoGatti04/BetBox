2a78ef4819a4d07a7d7c5120edaf1d9265953a0e
Simone Gatti <simogatti004@gmail.com>
2024-08-11 18:23:12 +0200

    pulizia file temporanei

--- betboxscrapernodejs/src/index.js
+++ betboxscrapernodejs/src/index.js
@@ -2,6 +2,7 @@ require('dotenv').config(); // Importa e configura dotenv
 const express = require('express');
 const balanceRoutes = require('./routes/balanceRoutes');
 const dailySpinRoutes = require('./routes/dailySpinRoutes');
+const {cleanupResources} = require("./services/cleanupService");
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -11,3 +12,5 @@ app.use('/spin', dailySpinRoutes);
 app.listen(port, () => {
   console.log(`Server in ascolto sulla porta ${port}`);
 });
+
+setInterval(cleanupResources, 6 * 60 * 60 * 1000);
