54098dcc2a701453840dcc48aa4c3a41d346d998
Simone Gatti <simogatti004@gmail.com>
2024-08-29 16:51:07 +0200

    cancellata app react

--- React_APP/src/siteImages.js
+++
@@ -1,9 +0,0 @@
-export const siteImages = {
-  snai: require('../assets/siteIcons/Snai.imageset/Snai.png'),
-  goldbet: require('../assets/siteIcons/Goldbet.imageset/Goldbet.png'),
-  lottomatica: require('../assets/siteIcons/Lottomatica.imageset/Better.png'),
-  bet365: require('../assets/siteIcons/Bet365.imageset/Bet365.png'),
-  eurobet: require('../assets/siteIcons/Eurobet.imageset/Eurobet.png'),
-  sisal: require('../assets/siteIcons/Sisal.imageset/Sisal.png'),
-  cplay: require('../assets/siteIcons/Cplay.imageset/Cplay.png')
-}
