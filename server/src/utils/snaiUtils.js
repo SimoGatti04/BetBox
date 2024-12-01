d8525de58de9296641c8bc46a28f6444d507564e
Simone Gatti <simogatti004@gmail.com>
2024-12-01 16:57:14 +0100

    blocca elementi

--- server/src/utils/snaiUtils.js
+++ server/src/utils/snaiUtils.js
@@ -41,27 +41,32 @@ async function snaiLogin(page){
   }
 
   if (!isUserLoggedIn){
-    console.log('Clic sul pulsante "Accedi"');
-    await page.locator('button.Header_btnLogin__O68th').waitFor({ state: 'visible', timeout: 120000});
-    await page.click('button.Header_btnLogin__O68th', {timeout: 120000});
+    try{
+      console.log('Clic sul pulsante "Accedi"');
+      await page.locator('button.Header_btnLogin__O68th').waitFor({ state: 'visible', timeout: 10000});
+      await page.click('button.Header_btnLogin__O68th', {timeout: 10000});
+    } catch (error) {
+      console.log('Errore durante il clic sul pulsante "Accedi": ', error);
+      isUserLoggedIn = true;
+    }
+    if (!isUserLoggedIn) {
+      console.log('Attesa del campo username');
+      await page.waitForSelector('input[name="username"]', {state: 'visible'});
 
-    console.log('Attesa del campo username');
-    await page.waitForSelector('input[name="username"]', { state: 'visible' });
+      console.log('Inserimento username');
+      await simulateTyping(page, 'input[name="username"]', config.snai.username);
 
-    console.log('Inserimento username');
-    await simulateTyping(page, 'input[name="username"]', config.snai.username);
+      console.log('Inserimento password');
+      await simulateTyping(page, 'input[name="password"]', config.snai.password);
 
-    console.log('Inserimento password');
-    await simulateTyping(page, 'input[name="password"]', config.snai.password);
+      console.log('Attesa del pulsante di invio accesso');
+      await page.waitForSelector('div.Button_childrenContainer__YUfnj', {state: 'visible'});
 
-    console.log('Attesa del pulsante di invio accesso');
-    await page.waitForSelector('div.Button_childrenContainer__YUfnj', { state: 'visible' });
+      console.log('Clic sul pulsante di invio accesso');
+      await page.click('div.Button_childrenContainer__YUfnj', {timeout: 10000});
 
-    console.log('Clic sul pulsante di invio accesso');
-    await page.click('div.Button_childrenContainer__YUfnj', {timeout: 120000});
-
-    await delay (3000, 4000);
-
+      await delay(3000, 4000);
+    }
   }
   console.log('Clic sul pulsante dashboard');
   await page.waitForSelector('div.UserNavigation_btnLinkContainer__Lc20b button.UserNavigation_btnLink__vk3Hf', {timeout: 120000});
