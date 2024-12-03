693dbc211cf389ccfb3934808c6b0fc365320324
Simone Gatti <simogatti004@gmail.com>
2024-12-03 02:51:16 +0100

    bot browsers

--- server/config.js
+++ server/config.js
@@ -1,14 +1,9 @@
-const headless = true;
+const headless = false;
 const record = false;
 
-const botEngines = {
-    bet365: 'playwright',
-    cplay: 'playwright',
-    eurobet: 'playwright',
-    goldbet: 'playwright',
-    lottomatica: 'playwright',
-    sisal: 'playwright',
-    snai: 'playwright',
-}
+const browsers = {
+    snai: 'webkit',
+};
 
-module.exports = { headless, record, botEngines };
+
+module.exports = { headless, record, browsers };
